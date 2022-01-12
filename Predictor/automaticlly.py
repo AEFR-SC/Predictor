64b21fd082ecc30cddd9493df6302847b0d60e76
Choctocatina <wsfanjianing@163.com>
2022-01-12 21:48:35 +0800

    Let something better

--- Predictor/automaticlly.py
+++ Predictor/automaticlly.py
@@ -1,20 +1,7 @@
 import easygui as eg
 import data_writer
 import predictor
-import decimal
 
-def half_up(x):
-    decimal.getcontext().prec = 4
-    ix = decimal.Decimal(int(x))
-    ix2 = decimal.Decimal(ix+1)
-    x = decimal.Decimal(x)
-    dx_up = abs(ix2-x)
-    dx_down = abs(ix-x)
-    if dx_up <= dx_down:
-        r = ix2
-    else:
-        r = ix
-    return int(r)
 
 def main():
     m_b = eg.boolbox
@@ -23,8 +10,7 @@ def main():
     detail__ = m_b("Do you want to show more details?", choices=("[Y]es", "[N]o"))
     real_result = data_writer.main(times, detail__)
     result_after_training = predictor.main(detail__)
-    bool_return = real_result == half_up(result_after_training)
-    print(real_result, half_up(result_after_training))
+    bool_return = abs(real_result-result_after_training)<=1
     if bool_return:
         print("TRUE.")
     else:
